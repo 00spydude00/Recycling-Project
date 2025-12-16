22231c1178e652ec1f35d5ae3ef1c20c373bea5e
spydude <00spydude00@gmail.com>
2025-12-16 13:43:40 -0700

    change to flask, begin proper backend, add index.html template.

---
+++ backend/app.py
@@ -0,0 +1,22 @@
+import json
+from pathlib import Path
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+DATA_PATH = Path(__file__).parent / "data" / "recycling-rules.json"
+
+with open(DATA_PATH) as f:
+    recycling_data = json.load(f)
+
+@app.route("/", methods=["GET"])
+def show_homepage():
+    return render_template("index.html")
+# city_input = input("Which city? (Utah county only)").title()
+
+# if city_input not in recycling_data:
+    # print("City not found.")
+
+# else:
+    # user_city_rules = recycling_data[city_input]
+    # print(user_city_rules)
